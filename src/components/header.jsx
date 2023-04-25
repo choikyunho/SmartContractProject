bca745cc0cb79b137a8e0922a9414cb117f49f83
choikyunho <rbsgh777@naver.com>
2023-04-25 13:45:39 +0900

    Header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,8 +2,26 @@ import { FaChessRook } from "react-icons/fa";
 import { BiWallet } from "react-icons/bi";
 import { AiFillHeart } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Header = ({ account, setAccount }) => {
+  const [coinprice,setCoinPrice]=useState();
+  const getCoinPrice = async()=>{
+    
+    try{
+        const response = await axios.get('https://api.upbit.com/v1/ticker?markets=KRW-BTC,%20KRW-MATIC,%20KRW-xrp');
+
+        console.log(response);
+        setCoinPrice([
+            {Symbol:"BTC",price:response.data[0].trade_price},
+            {Symbol:"MATIC",price:response.data[1].trade_price},        
+            {Symbol:"XRP",price:response.data[2].trade_price},   
+        ])
+    }catch(error){
+        console.error(error);
+    }}
+
   const onClickAccount = async () => {
     try {
       const accounts = await window.ethereum.request({
@@ -15,6 +33,10 @@ const Header = ({ account, setAccount }) => {
       console.error(error);
     }};
 
+    useEffect(() => {
+        getCoinPrice();
+      }, []);
+
   return (
     <header className="max-w-screen-xl mx-auto p-4 flex justify-between items-center font-bold">
       <Link to="/">
@@ -23,19 +45,28 @@ const Header = ({ account, setAccount }) => {
           <div className="ml-1 text-xl">Ble-Chess</div>
         </div>
       </Link>
-      <div>
+        <div>
+         {coinprice && (
+            <ul>
+                {coinprice.map((v,i)=>{
+                    return(
+                        <li key={i} className="ml-2">
+                          {v.Symbol}: {(v.price/1000).toLocaleString()}KW
+                        </li>);})}
+            </ul>)}
+
          {account ? 
          (
           <div className="flex items-center p-2 bg-gray-800 rounded-full">
               <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center">
                 <AiFillHeart />
               </div>
-              <div className="ml-1">
+              <div className="ml-2">
                 {account.substring(0, 4)}... {account.substring(account.length - 4)}
               </div>
           </div>
          ) : (
-          <button className="flex items-center p-2 bg-gray-800 rounded-full"onClick={onClickAccount}>
+          <button className="flex items-center p-2 bg-gray-800 rounded-full m-4"onClick={onClickAccount}>
             <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center">
               <BiWallet />
             </div>
